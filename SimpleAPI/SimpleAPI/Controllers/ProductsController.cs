3c43ca9e54ba95d266cfd92b1d1b4fac12edebfc
Pavel Khodos <pkhodos@odin.com>
2017-07-18 18:04:36 +0300

    FIRST Commit
    FIRST Commit

---
+++ SimpleAPI/SimpleAPI/Controllers/ProductsController.cs
@@ -0,0 +1,73 @@
+﻿using OAuth;
+using SimpleAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Http;
+
+
+namespace SimpleAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+
+
+            return products;
+
+
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            /*
+            var crypto = new HMACSHA1();
+            var key = "{0}&{1}".FormatWith("", "");
+
+            crypto.Key = _encoding.GetBytes(key);
+            signature = signatureBase.HashWith(crypto);
+
+            break;
+            
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(requestUrl);
+            
+
+
+            OAuthBase oauthBase = new OAuthBase();
+            string uri = "https://api-isv-oa.aps.odin.com/aps/2/resources/";
+            string consumerKey = "blablaservice-v1-ad95e2443d814b8";
+            string consumerSecret = "63473882d7d74aeb97d2ea17caff8f52";
+            string timestamp = oauthBase.GenerateTimeStamp();
+            string nonce = oauthBase.GenerateNonce();
+            string normalizedUrl;
+            string normalizedRequestParameters;
+            string sig = HttpUtility.UrlEncode(oauthBase.GenerateSignature(
+                new Uri(uri), consumerKey, consumerSecret, string.Empty, string.Empty,
+                "GET", timestamp, nonce, out normalizedUrl, out normalizedRequestParameters));
+            string requestUrl = String.Format("{0}?{1}&oauth_signature={2}", normalizedUrl, normalizedRequestParameters, sig);
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(normalizedUrl);
+            request.Headers.Add("Authorization", String.Format("OAuth {0}", normalizedRequestParameters));
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            */
+            product.Name = "PASHA";// requestUrl;// response.ToString();
+            return Ok(product);
+        }
+    }
+}
---
+++ SimpleConsoleAPI/SimpleConsoleAPI/Program.cs
@@ -0,0 +1,74 @@
+﻿using OAuth;
+using System;
+using System.Net;
+using System.Web;
+using System.IO;
+
+namespace SimpleConsoleAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OAuthBase oauthBase = new OAuthBase();
+            string uri = "https://api-isv-oa.aps.odin.com/aps/2/resources/";
+            string consumerKey = "blablaservice-v1-ad95e2443d814b8";
+            string consumerSecret = "63473882d7d74aeb97d2ea17caff8f52";
+            string timestamp = oauthBase.GenerateTimeStamp();
+            string nonce = oauthBase.GenerateNonce();
+
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.ContentType = "application/json";
+            request.Method = WebRequestMethods.Http.Get; // change it to  WebRequestMethods.Http.Post if needed
+
+            string normalizedUrl;
+            string normalizedRequestParameters;
+
+            // generating signature based on requst parameters
+            string sig = HttpUtility.UrlEncode(
+                oauthBase.GenerateSignature(
+                    new Uri(uri),
+                    consumerKey, 
+                    consumerSecret, 
+                    string.Empty, string.Empty,
+                    request.Method, 
+                    timestamp, 
+                    nonce, 
+                    out normalizedUrl, out normalizedRequestParameters));
+
+            request.Headers.Add("Authorization", String.Format(
+               "OAuth " +
+                "oauth_consumer_key=\"{0}\"" +
+               ",oauth_signature_method=\"HMAC-SHA1\"" +
+               ",oauth_timestamp=\"{1}\"" +
+               ",oauth_nonce=\"{2}\"" +
+               ",oauth_version=\"1.0\"" +
+               ",oauth_signature=\"{3}\"",
+               consumerKey, timestamp, nonce, sig
+               ));
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    Console.WriteLine(new StreamReader(response.GetResponseStream()).ReadToEnd());
+                }
+            }
+            catch (WebException e)
+            {
+                using (WebResponse response = e.Response)
+                {
+                    HttpWebResponse httpResponse = (HttpWebResponse)response;
+                    Console.WriteLine("Error code: {0}", httpResponse.StatusCode);
+                    using (Stream data = response.GetResponseStream())
+                    using (var reader = new StreamReader(data))
+                    {
+                       string text = reader.ReadToEnd();
+                       Console.WriteLine(text);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
